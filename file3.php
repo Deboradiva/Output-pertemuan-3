fc9ff9333923f8a8924261e898ae14e6cca38e89
Deboradiva <deboradivaningrum2003@gmail.com>
2022-09-05 20:14:21 +0700

    output pertemuan 3

---
+++ file3.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();
+    echo $_SESSION['Nama Lengkap'];
+    echo "<br>";
+    echo $_SESSION['Nama Panggilan'];
+    echo "<br>";
+    echo $_SESSION['NIM'];
+    echo "<br>";
+    echo $_SESSION['Fakultas'];
+    echo "<br>";
+    echo $_SESSION['Alamat Rumah'];
+    echo "<br>";
+    echo $_SESSION['Jenis Kelamin'];
+    echo "<br>";
+    
+?>
